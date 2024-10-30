8bac2cc8b4a5847cc374efb6940807f3f90e0a1d
ardkoornstra <apckoornstra@gmail.com>
2024-10-30 13:16:03 +0100

    Migrations and controller

---
+++ LatijnBackend/LatijnBackend/Controllers/WerkwoordenController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LatijnBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WerkwoordenController : ControllerBase
+    {
+        // GET: api/<WerkwoordenController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<WerkwoordenController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
--- LatijnBackend/LatijnBackend/Program.cs
+++ LatijnBackend/LatijnBackend/Program.cs
@@ -1,7 +1,11 @@
+using LatijnBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+string connectionstring = builder.Configuration["LatijnDB"];
+builder.Services.AddDbContext<LatijnContext>(options => options.UseMySql(connectionstring, ServerVersion.AutoDetect(connectionstring)));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
