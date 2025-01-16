c3d9c2ca5347c8b256c8c238e0e606ef39f260a9
ardkoornstra <apckoornstra@gmail.com>
2025-01-16 21:22:15 +0100

    test setup

---
+++ LatijnBackend/LatijnTest/Services/SessionsLogicTests.cs
@@ -0,0 +1,77 @@
+﻿using LatijnLogic.Interfaces;
+using LatijnLogic.Services;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LatijnTest.Services
+{
+    public class SessionsLogicTests
+    {
+        private MockRepository mockRepository;
+
+        private Mock<ISessionsData> mockSessionsData;
+
+        public SessionsLogicTests()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+            this.mockSessionsData = this.mockRepository.Create<ISessionsData>();
+        }
+
+        private SessionsLogic CreateSessionsLogic()
+        {
+            return new SessionsLogic(
+                this.mockSessionsData.Object);
+        }
+
+        [Fact]
+        public async Task GetSession_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var sessionsLogic = this.CreateSessionsLogic();
+            int id = 0;
+
+            // Act
+            var result = await sessionsLogic.GetSession(
+                id);
+
+            // Assert
+            Assert.True(false);
+            this.mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public async Task GetSessionsByDocent_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var sessionsLogic = this.CreateSessionsLogic();
+            int docentId = 0;
+
+            // Act
+            var result = await sessionsLogic.GetSessionsByDocent(
+                docentId);
+
+            // Assert
+            Assert.True(false);
+            this.mockRepository.VerifyAll();
+        }
+
+        [Fact]
+        public async Task CreateSession_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var sessionsLogic = this.CreateSessionsLogic();
+            int docentId = 0;
+
+            // Act
+            var result = await sessionsLogic.CreateSession(
+                docentId);
+
+            // Assert
+            Assert.True(false);
+            this.mockRepository.VerifyAll();
+        }
+    }
+}
--- LatijnBackend/LatijnTest/UnitTest1.cs
+++
@@ -1,8 +0,0 @@
-namespace LatijnTest {
-    public class UnitTest1 {
-        [Fact]
-        public void Test1() {
-            Assert.Equal(1, 1);
-        }
-    }
-}
